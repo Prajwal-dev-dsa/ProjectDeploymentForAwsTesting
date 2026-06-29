569eebf33682b63dac9ff56cd852530577e0e8d6
Prajwal <prajwal77dwivedi@gmail.com>
2026-06-29 12:38:52 +0530

    CI/CD Commit 3

--- index.js
+++ index.js
@@ -8,11 +8,11 @@ const PORT = process.env.PORT || 8000
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("Hello World!");
+    res.send("Hello World! Now you are seeing the changes after cicd integration!");
 });
 
 app.get("/check", (req, res) => {
-    res.send("Server is running!");
+    res.send("Server is running! CICD is working!");
 });
 
 app.listen(PORT, () => {
